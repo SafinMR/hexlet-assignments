e92791695c211c826ad08c4708551234cd70ac74
@hexlet/cli <support@hexlet.io>
2023-10-04 00:56:33 +0300

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/test/java/exercise/App.java
@@ -0,0 +1,32 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static boolean scrabble(String setOfChars, String word) {
+        setOfChars = setOfChars.toLowerCase();
+        word = word.toLowerCase();
+
+        List<String> charsList = new ArrayList<>();
+        char[] setOfCharsArray = setOfChars.toCharArray();
+        for (char l : setOfCharsArray) {
+            charsList.add(String.valueOf(l));
+        }
+
+        List<String> wordCharsList = new ArrayList<>();
+        char[] wordArray = word.toCharArray();
+        for (char w : wordArray) {
+            wordCharsList.add(String.valueOf(w));
+        }
+
+        for (String w : wordCharsList) {
+            if (!charsList.contains(w)) {
+                return false;
+            }
+            charsList.remove(w);
+        }
+
+        return true;
+    }
+}
